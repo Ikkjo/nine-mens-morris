6e241feb5f23707d1961c2061c1692b012def998
Ilija Kalinić <ilija.kalinic@gmail.com>
2020-04-13 16:41:26 +0200

    Worked on printing game board in game_board.py

--- game_board.py
+++ game_board.py
@@ -1,67 +1,70 @@
-def print_game_board():
-    board = """●————————————————————●————————————————————●
+class GameboardPositions(object):
+
+    def __init__(self, positions):
+        self._array = ["●" for position in range(positions)]
+        self._num_of_positions = positions
+
+    def __str__(self):
+        return str(self._array)
+
+    @property
+    def array(self):
+        return self._array
+
+    @property
+    def num_of_positions(self):
+        return self._num_of_positions
+
+
+
+
+def print_game_board(position_array):
+
+    board = f"""
+{position_array[0]}————————————————————{position_array[1]}————————————————————{position_array[2]}
 |                    |                    |
 |                    |                    |
-|      ●—————————————●—————————————●      |
+|      {position_array[3]}—————————————{position_array[4]}—————————————{position_array[5]}      |
 |      |             |             |      |
 |      |             |             |      |
-|      |      ●——————●——————●      |      |
+|      |      {position_array[6]}——————{position_array[7]}——————{position_array[8]}      |      |
 |      |      |             |      |      |
 |      |      |             |      |      |
-●——————●——————●             ●——————●——————●
+{position_array[9]}——————{position_array[10]}——————{position_array[11]}             {position_array[12]}——————{position_array[13]}——————{position_array[14]}
 |      |      |             |      |      |
 |      |      |             |      |      |
-|      |      ●——————●——————●      |      |
+|      |      {position_array[15]}——————{position_array[16]}——————{position_array[17]}      |      |
 |      |             |             |      |
 |      |             |             |      |
-|      ●—————————————●—————————————●      |
+|      {position_array[18]}—————————————{position_array[19]}—————————————{position_array[20]}      |
 |                    |                    |
 |                    |                    |
-●————————————————————●————————————————————●"""
+{position_array[21]}————————————————————{position_array[22]}————————————————————{position_array[23]}"""
+
+    between_3 = f"{'|':<21}|{'|':>21}"
+
+    between_5 = f"{'|':<7}{'|':<14}|{'|':>14}{'|':>7}"
+
+    between_7 =f"{'|':<7}{'|':<7}{'|':<14}{'|':>7}{'|':>7}"
+
+    long_3_in_row = f""
+
+    medium_3_in_row = f""
+
+    small_3_in_row = f""
+
+    six_in_row = f""
+
+
 
     print(board)
-
-def print_box(side):
-
-
-
-    for row in range(side):
-
-        for column in range(side):
-            if (row == 0 or row == side-1) or (column == 0 or column == side-1):
-                if row == 0:
-                    if column == 0:
-                        print("┏", end='')
-
-                    elif column == side-1:
-                        print("┓", end="\n")
-
-                    else:
-                        print("━", end='')
-
-                elif row == side-1:
-                    if column == 0:
-                        print("┗", end='')
-
-                    elif column == side-1:
-                        print("┛")
-
-                    else:
-                        print("━", end='')
-
-                else:
-                    if column == 0 or column == side-1:
-
-                        end = '\n' if column == side - 1 else ''
-
-                        print("┃", end=end)
-
-                    print(" "*side, end='')
-
-
-
-
+    # "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O",
+    # "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O",
+    # "O", "O",
 
 
 if __name__ == '__main__':
-    print_box(20)
+
+    gameboard = GameboardPositions()
+
+    print_game_board(gameboard.array)
