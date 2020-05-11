d58a32236cb33bf8c272ae7af0e2bb25e1038d37
Ilija Kalinić <ilija.kalinic@gmail.com>
2020-05-11 19:05:00 +0200

    Update player.py

--- modules/player.py
+++ modules/player.py
@@ -1,12 +1,20 @@
 from modules.piece import Piece
 
-class Player(object):
+
+class HumanPlayer(object):
 
     starting_pieces = 9
 
-    def init(self, color):
+    def __init__(self, color: int):
         piece_list = list()
 
         for piece in range(self.starting_pieces):
 
-            piece_list.append(Piece())
+            piece_list.append(Piece(color))
+
+        self.pieces_unused_list = piece_list
+        self.piece_list = list()
+
+    @property
+    def pieces_left(self):
+        return len(self.piece_list) + len(self.pieces_unused_list)
