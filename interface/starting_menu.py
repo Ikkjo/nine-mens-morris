69048c2c0fc830f9a924e772b21b8f7a231621b1
Ilija Kalinić <ilija.kalinic@gmail.com>
2020-05-12 10:10:12 +0200

    Added main.py Changed ascii_art.py Updated starting_menu.py

--- interface/starting_menu.py
+++ interface/starting_menu.py
@@ -1,26 +1,25 @@
-from interface.ascii_art import art
-from interface.exit_check import exit
-from interface.start_game import start_game
-from interface.clear_screen import clear_screen
 from time import sleep
+
+from interface.ascii_art import art
+from interface.clear_screen import clear_screen
+from interface.exit_check import exit
 
 
 def splash_screen():
-    made_by = "Ilija Kalinic"
+    made_by = "Ilija Kalinić"
     game_name = "Nine Men's Morris"
-    print("{0:=^60}\nMade by:{1:=>52}".format(game_name, made_by))
-    print(art)
+    print("{0:#^60}\nMade by:{1:#>52}".format(game_name, made_by))
+    print("{:^60}".format(art))
 
 
 def print_game_options():
-    print("{:=^60}".format("Choose which type of game do you want to play:") +
-          "\n\n1 - Human vs Human\n\n"
-          "2 - Human vs Bot\n\n"
-          "Enter - exit")
+    print("|>{:^60}<|".format("Choose which type of game you want to play") +
+          "\n\n" + "{:^60}".format("1 - Human vs Human") + "\n\n"
+          "{:^60}".format("2 - Human vs Bot") + "\n\n\n\n"
+          "{:>60}".format("Enter - exit") + "\n\n")
 
 
 def starting_menu():
-
     options = {"1": "human",
                "2": "bot"}
 
